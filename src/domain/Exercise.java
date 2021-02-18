f45d4169a5cc8ac8d2b0cb8ba89d19f1a2534628
Kostya0777 <72386928+Kostya0777@users.noreply.github.com>
2021-02-18 18:26:18 +0200

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,29 @@
 package domain;
 
+/**
+ * Клас який представляє завдання
+ * @author Kostya0777
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
+    
+    /**
+     * Повертає всі дільники числа
+     * @param number число
+     * @return рядок дільників через кому
+     */
+    public static String Calculate(int number) {
+        String res = "";
+        
+        if ((String.valueOf(Math.abs(number)).length() != 6) || number < 0) {
+            return "number incorrect";
+            }
+        
+        
+        for (int i = 1; i <= number; i++) {
+        if(0 == (number % i))
+            res += i + ",";
+        }
+        return res;
+}
+    
 }
